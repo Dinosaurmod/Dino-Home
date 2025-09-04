9c0ad33455b0298402459a76133b18613b466445
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-09-04 15:16:06 +0200

    some more fixes

--- src/components/menu-bar/search-bar.jsx
+++ src/components/menu-bar/search-bar.jsx
@@ -10,7 +10,7 @@ const SearchBarComponent = function () {
     return (
         <React.Fragment>
             <div className="searchbar-main-div">
-                <button id="searchbar-searchIcon" id="searchbar-searchIcon"><svg xmlns="http://www.w3.org/2000/svg" style={{width: "30px", height: "20px", marginBottom:"5px", marginTop: "5px"}} viewBox="0 0 19.9 19.7"><g class="search-path" fill="none" stroke="#ffffff" stroke-width="2px"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"></path><circle cx="8" cy="8" r="7"></circle></g></svg></button>
+                <button class="searchbar-searchIcon" id="searchbar-searchIcon"><svg xmlns="http://www.w3.org/2000/svg" style={{width: "30px", height: "20px", marginBottom:"5px", marginTop: "5px"}} viewBox="0 0 19.9 19.7"><g class="search-path" fill="none" stroke="#ffffff" stroke-width="2px"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"></path><circle cx="8" cy="8" r="7"></circle></g></svg></button>
                 <input type="text" className="searchbar-searchInput" id="searchbar-searchInput" placeholder="Search for projects on PM..."/>
             </div>
         </React.Fragment>
