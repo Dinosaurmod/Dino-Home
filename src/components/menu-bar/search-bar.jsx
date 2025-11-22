15e75bdb842deaea3e99dec27d2b78bfa4e4a574
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-11-22 14:57:22 +0100

    darn it

--- src/components/menu-bar/search-bar.jsx
+++ src/components/menu-bar/search-bar.jsx
@@ -23,7 +23,7 @@ const SearchBarComponent = function (props) {
         <React.Fragment>
             <div className="searchbar-main-div">
                 <button class="searchbar-searchIcon" id="searchbar-searchIcon"><svg xmlns="http://www.w3.org/2000/svg" style={{width: "30px", height: "20px", marginBottom:"5px", marginTop: "5px"}} viewBox="0 0 19.9 19.7"><g class="search-path" fill="none" stroke="#ffffff" stroke-width="2px"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"></path><circle cx="8" cy="8" r="7"></circle></g></svg></button>
-                <input type="text" className="searchbar-searchInput" id="searchbar-searchInput" placeholder={Utils.formatMessage(searchText, intl, "Search for projects on PM...")}/>
+                <input type="text" className="searchbar-searchInput" id="searchbar-searchInput" placeholder={Utils.formatMessage(messages.searchText, intl, "Search for projects on PM...")}/>
             </div>
         </React.Fragment>
     )
