ffe0030e8ad5104024b7283bb5c8a844529b0227
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2026-01-02 18:16:21 +0100

    aaaa

--- src/components/whats-new/whats-new-data.js
+++ src/components/whats-new/whats-new-data.js
@@ -1,7 +1,9 @@
+import blockFonts from './blockfonts.gif'
+
 export default {
     message: "You can now change the fonts of blocks and dropdowns with an addon!",
     author: "By Gabs The Mr Incredible Maker (GabsTheCuriousKid)",
-    picture: "./blockfonts.gif",
+    picture: blockFonts,
     avatarUrl: "https://avatars.githubusercontent.com/u/140965985?v=4&size=40",
     secondMessage: "Sorry for the awful quality :(",
     pictureData: {
--- webpack.config.js
+++ webpack.config.js
@@ -20,13 +20,13 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.(svg|png|jpg|ico|woff|woff2|eot|ttf|otf)$/,
+                test: /\.(svg|png|jpg|ico|woff|woff2|eot|ttf|otf|gif)$/,
                 type: 'asset/resource'
             },
         ],
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.css', '.svg', '.png', '.jpg', '.ico', '.woff', '.woff2', '.eot', '.ttf', '.otf']
+        extensions: ['.js', '.jsx', '.css', '.svg', '.png', '.jpg', '.ico', '.woff', '.woff2', '.eot', '.ttf', '.otf', '.gif']
     },
     plugins: [
         new HtmlWebpackPlugin({
